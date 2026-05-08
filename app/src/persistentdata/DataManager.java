4c46b314d6827d1037ba55a72f18b57941c871bd
u8153266 <u8153266@anu.edu.au>
2026-05-08 15:51:12 +1000

    update func

--- app/src/persistentdata/DataManager.java
+++ app/src/persistentdata/DataManager.java
@@ -23,44 +23,39 @@ public class DataManager {
 
 	private final IOFactory IO = new ComputerIOFactory();
 
-	// We have assumed that most solutions to the serialization task in week-5 will
-	// use a 4-column schema for Users. If this is not the case, you may need to
-	// change the number below.
 	private final DataPipeline<User, String[]> userPipeline = new DataPipeline<>(
 			IO, new CSVFormattedFactory(new CSVFormat(4)), new UserSerializer(), "users");
 
 	private final DataPipeline<Post, String[]> postPipeline = new DataPipeline<>(
 			IO, new CSVFormattedFactory(new CSVFormat(3)), new PostSerializer(), "posts");
 
+	// 6 columns now: id, poster, thread, timestamp, message text, hidden flag
 	private final DataPipeline<Message, String[]> messagePipeline = new DataPipeline<>(
-			IO, new CSVFormattedFactory(new CSVFormat(5)), new MessageSerializer(), "messages");
+			IO, new CSVFormattedFactory(new CSVFormat(6)), new MessageSerializer(), "messages");
 
-	// Applies to reports and hidden messages as well.
 	private final DataPipeline<Report, String[]> reportPipeline = new DataPipeline<>(
 			IO, new CSVFormattedFactory(new CSVFormat(3)), new ReportSerializer(), "reports");
 
-	private final DataPipeline<UUID, String[]> hiddenPipeline = new DataPipeline<>(
-			IO, new CSVFormattedFactory(new CSVFormat(1)), new HiddenMessageSerializer(), "hidden");
-
-
 	private final UserDAO users = UserDAO.getInstance();
 	private final PostDAO posts = PostDAO.getInstance();
+	private final ReportDAO reports = ReportDAO.getInstance();
 
 	public void readAll() {
 		users.clear();
 		posts.clear();
-		reportStore.clear();
-		hiddenStore.clear();
+		reports.clear();
+
 		userPipeline.readTo(users::add);
 		postPipeline.readTo(posts::add);
-		messagePipeline.readTo((message) -> posts.get(new Post(message.thread())).messages.insert(message));
+		messagePipeline.readTo((message) ->
+				posts.get(new Post(message.thread())).messages.insert(message));
+		reportPipeline.readTo(reports::addExisting);
 	}
 
 	public void writeAll() {
 		userPipeline.writeFrom(users.getAll());
 		postPipeline.writeFrom(posts.getAll());
 		messagePipeline.writeFrom(posts.getAllMessages());
-		reportPipeline.writeFrom(reportStore.getAllReports());
-		hiddenPipeline.writeFrom(hiddenStore.getAll());
+		reportPipeline.writeFrom(reports.allReports());
 	}
 }
